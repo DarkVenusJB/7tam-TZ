c363c9c3a285c1cad5aefcc85c983bd1780e316f
Dmitriy Olehnovich <91538380+DarkVenusJB@users.noreply.github.com>
2025-05-29 02:45:04 +0300

    scaled cooliders

---
+++ Assets/Scripts/ScreenEdgeColliders.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+
+namespace Scripts
+{
+    public class ScreenEdgeColliders : MonoBehaviour
+    {
+        [SerializeField] private BoxCollider2D _bottomEdge;
+        [SerializeField] private BoxCollider2D _leftEdge;
+        [SerializeField] private BoxCollider2D _rightEdge;
+        
+        private Camera _camera;
+
+        private void Start()
+        {
+            _camera = Camera.main;
+            
+            UpdateEdges();
+        }
+        
+        void UpdateEdges()
+        {
+            float height = 2f * _camera.orthographicSize;
+            float width = height * _camera.aspect;
+
+            Vector3 camPos = _camera.transform.position;
+            
+            _bottomEdge.transform.position = new Vector3(camPos.x, camPos.y - height / 2f - _bottomEdge.transform.localScale.y / 2f, 0);
+            _leftEdge.transform.position = new Vector3(camPos.x - width / 2f - _leftEdge.transform.localScale.x / 2f, camPos.y, 0);
+            _rightEdge.transform.position = new Vector3(camPos.x + width / 2f + _rightEdge.transform.localScale.x / 2f, camPos.y, 0);
+            
+            //_bottomEdge.size = new Vector2(width + _bottomEdge.transform.localScale.x * 2, _bottomEdge.transform.localScale.x);
+            //_leftEdge.size = new Vector2(_bottomEdge.transform.localScale.x, height + _bottomEdge.transform.localScale.x * 2);
+            //_rightEdge.size = new Vector2(_bottomEdge.transform.localScale.x, height + _bottomEdge.transform.localScale.x * 2);
+        }
+    }
+}
